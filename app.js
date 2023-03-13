e1d0c1d8f8fe6abdf676ba11ea1435c9f2960ee1
LeylaNj <126622311+LeylaNj@users.noreply.github.com>
2023-03-14 00:24:58 +0100

    add api to date and time

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+function updateDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let today = date.getDate();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+
+  return `${day}  ${today} ${month} ${hours}:${minutes}`;
+}
+
 function displayTemp(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +50,8 @@ function displayTemp(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
   let pressureElement = document.querySelector("#pressure");
   pressureElement.innerHTML = `${response.data.temperature.pressure} hPa`;
+  let dateElement = document.querySelector("#date-time");
+  dateElement.innerHTML = updateDate(response.data.time * 1000);
 }
 
 let apiKey = "0tade3eeb77402aa34ff8a204c86adoc";
